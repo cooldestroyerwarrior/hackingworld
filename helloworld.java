774748a5320d752f82b2104c82c5d9a436ce3048
cooldestroyerwarrior <148484606+cooldestroyerwarrior@users.noreply.github.com>
2023-10-19 23:01:55 +0530

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,11 @@
+
+public class helloworlds {
+
+	public static void main(String[] args) {
+		System.out.println("HEllo WoRlD");
+		System.out.println("Learning Java Now");
+		System.out.println("Programming is Fun");
+
+	}
+
+}
